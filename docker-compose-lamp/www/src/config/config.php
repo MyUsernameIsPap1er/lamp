7fa8691cd69cec726c806829476c21f45bfa1eaa
MyUsernameIsPap1er <maximeadam1999@hotmail.com>
2025-09-17 14:29:16 -0400

    specifications1.1

--- docker-compose-lamp/www/src/config/config.php
+++ docker-compose-lamp/www/src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Read docker-compose LAMP environment (sprintcube uses .env)
 // In containers, hostname for MySQL is usually "mysql"
-define('DB_HOST', getenv('MYSQL_HOST') ?: 'mysql');
-define('DB_NAME', getenv('MYSQL_DATABASE') ?: 'app');
-define('DB_USER', getenv('MYSQL_USER') ?: 'root');
-define('DB_PASS', getenv('MYSQL_PASSWORD') ?: (getenv('MYSQL_ROOT_PASSWORD') ?: 'root'));
+define('DB_HOST', getenv('MYSQL_HOST') );
+define('DB_NAME', getenv('MYSQL_DATABASE') );
+define('DB_USER', getenv('MYSQL_USER') );
+define('DB_PASS', getenv('MYSQL_PASSWORD') ?: (getenv('MYSQL_ROOT_PASSWORD')));
